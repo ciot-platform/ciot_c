ee2eec69d259270440384484e71c92773e1b09cc
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-25 10:32:28 -0300

    refactor: update MQTT client state retrieval and remove unused connection check

--- src/common/ciot_mqtt_client_base.c
+++ src/common/ciot_mqtt_client_base.c
@@ -98,21 +98,18 @@ ciot_err_t ciot_mqtt_client_set_subtopic(ciot_mqtt_client_t self, char *subtopic
     return CIOT_ERR_OK;
 }
 
-bool ciot_mqtt_client_is_connected(ciot_mqtt_client_t self)
+ciot_mqtt_client_state_t ciot_mqtt_client_get_state(ciot_mqtt_client_t self)
 {
     ciot_mqtt_client_base_t *base = (ciot_mqtt_client_base_t*)self;
-    return base->status.state == CIOT_MQTT_CLIENT_STATE_CONNECTED;
+    CIOT_ERR_NULL_CHECK(self);
+    return base->status.state;
 }
 
-ciot_err_t ciot_mqtt_client_set_will(ciot_mqtt_client_t self, ciot_mqtt_client_last_will_t *last_will)
-{
-    CIOT_ERR_NULL_CHECK(self);
-    CIOT_ERR_NULL_CHECK(last_will);  
-    ciot_mqtt_client_base_t *base = (ciot_mqtt_client_base_t*)self;
-    base->cfg.last_will = *last_will;
-    base->cfg.has_last_will = true;
-    return CIOT_ERR_OK;
-}
+// bool ciot_mqtt_client_is_connected(ciot_mqtt_client_t self)
+// {
+//     ciot_mqtt_client_base_t *base = (ciot_mqtt_client_base_t*)self;
+//     return base->status.state == CIOT_MQTT_CLIENT_STATE_CONNECTED;
+// }
 
 static ciot_err_t ciot_mqtt_client_process_data(ciot_iface_t *iface, ciot_msg_data_t *data)
 {
