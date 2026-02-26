39620d3c80bd9e3f8b9d72803b65189a0e3cf46f
wesley-santos <53940899+wjsan@users.noreply.github.com>
2026-02-26 08:32:27 -0300

    feat: update MQTT client to use dynamic topic size and implement publish functions with retain option

--- src/esp32/ciot_mqtt_client.c
+++ src/esp32/ciot_mqtt_client.c
@@ -45,25 +45,7 @@ ciot_err_t ciot_mqtt_client_start(ciot_mqtt_client_t self, ciot_mqtt_client_cfg_
     CIOT_ERR_NULL_CHECK(cfg);
     ciot_mqtt_client_base_t *base = &self->base;
 
-    if(cfg->has_last_will == false && base->cfg.has_last_will)
-    {
-        cfg->has_last_will = true;
-        cfg->last_will = base->cfg.last_will;
-    }
-
-    if(cfg->has_topics == false && base->cfg.has_topics)
-    {
-        cfg->has_topics = true;
-        cfg->topics = base->cfg.topics;
-    }
-
-    base->cfg = *cfg;
-    if (base->cfg.has_topics)
-    {
-        strcpy(base->topic_sub, base->cfg.topics.sub);
-        strcpy(base->topic_pub, base->cfg.topics.pub);
-        base->topic_len = strlen(base->cfg.topics.pub);
-    }
+    CIOT_ERR_RETURN(ciot_mqtt_client_base_start(base, cfg));
 
     const esp_mqtt_client_config_t mqtt_client_cfg = {
         .broker.address.uri = base->cfg.url,
@@ -110,6 +92,11 @@ ciot_err_t ciot_mqtt_client_sub(ciot_mqtt_client_t self, char *topic, int qos)
 
 ciot_err_t ciot_mqtt_client_pub(ciot_mqtt_client_t self, char *topic, uint8_t *data, int size, int qos)
 {
+    return ciot_mqtt_client_publish(self, topic, data, size, qos, false);
+}
+
+ciot_err_t ciot_mqtt_client_publish(ciot_mqtt_client_t self, char *topic, uint8_t *data, int size, int qos, bool retain)
+{
     CIOT_ERR_NULL_CHECK(self);
     CIOT_ERR_NULL_CHECK(topic);
     CIOT_ERR_NULL_CHECK(self->handle);
@@ -117,13 +104,13 @@ ciot_err_t ciot_mqtt_client_pub(ciot_mqtt_client_t self, char *topic, uint8_t *d
     int err = 0;
     if (qos == 0)
     {
-        err = esp_mqtt_client_publish(self->handle, topic, (char *)data, size, qos, false);
+        err = esp_mqtt_client_publish(self->handle, topic, (char *)data, size, qos, retain);
         if (err == 0)
             ciot_mqtt_client_update_data_rate(self, size);
     }
     else
     {
-        err = esp_mqtt_client_enqueue(self->handle, topic, (char *)data, size, qos, false, false);
+        err = esp_mqtt_client_enqueue(self->handle, topic, (char *)data, size, qos, retain, false);
         if (err == 0)
             ciot_mqtt_client_update_data_rate(self, size);
     }
