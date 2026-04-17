fbc333bd59173908475e81c0c64c2bc0780dd999
Wesley Santos <wesleypro37@gmail.com>
2026-04-17 10:49:29 -0300

    fix: refine event handling to process only raw tag data in DFU event handler

--- src/common/ciot_dfu_nrf.c
+++ src/common/ciot_dfu_nrf.c
@@ -612,7 +612,7 @@ static ciot_err_t ciot_dfu_nrf_event_handler(ciot_iface_t *sender, ciot_event_t 
         self->state = CIOT_DFU_NRF_STATE_SEND_PING;
     }
 
-    if (event->type == CIOT_EVENT_TYPE_DATA)
+    if (event->type == CIOT_EVENT_TYPE_DATA && event->which_data == CIOT_EVENT_DATA_RAW_TAG)
     {
         ciot_err_t err = ciot_dfu_nrf_process_response(self, event->raw.bytes, event->raw.size);
         if (err != CIOT_ERR_OK)
