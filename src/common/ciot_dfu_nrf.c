c9eab18ba6a096beb70dca84da4b97190ba9f230
Wesley Santos <wesleypro37@gmail.com>
2026-04-17 10:30:01 -0300

    fix: update event type check from CIOT_EVENT_TYPE_MSG to CIOT_EVENT_TYPE_DATA

--- src/common/ciot_dfu_nrf.c
+++ src/common/ciot_dfu_nrf.c
@@ -612,7 +612,7 @@ static ciot_err_t ciot_dfu_nrf_event_handler(ciot_iface_t *sender, ciot_event_t 
         self->state = CIOT_DFU_NRF_STATE_SEND_PING;
     }
 
-    if (event->type == CIOT_EVENT_TYPE_MSG)
+    if (event->type == CIOT_EVENT_TYPE_DATA)
     {
         ciot_err_t err = ciot_dfu_nrf_process_response(self, event->raw.bytes, event->raw.size);
         if (err != CIOT_ERR_OK)
