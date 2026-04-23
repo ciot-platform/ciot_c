5bbf033f587e8fdf9a112fd4a41307886ede1405
Wesley Santos <wesleypro37@gmail.com>
2026-04-23 12:20:08 -0300

    feat: implement WiFi Multi interface and related functionalities

--- src/core/ciot_iface.c
+++ src/core/ciot_iface.c
@@ -293,6 +293,8 @@ const char* ciot_iface_type_to_str(ciot_iface_type_t iface_type)
             return "MBUS_CLIENT";
         case CIOT_IFACE_TYPE_MBUS_SERVER:
             return "MBUS_SERVER";
+        case CIOT_IFACE_TYPE_WIFI_MULTI:
+            return "WIFI_MULTI";
         default:
             return "UNKNOWN";
         break;
