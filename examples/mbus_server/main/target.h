cfcc282b12a4b0ed5392af9f076d8a2a18a62bb8
wesley-santos <BINARY_QUANTUM@outlook.com>
2026-06-03 08:08:36 -0300

    feat(examples): added support to esp32 on mbus_server sample

---
+++ examples/mbus_server/main/target.h
@@ -0,0 +1,62 @@
+/**
+ * @file target.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-05-28
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#ifndef __TARGET__H__
+#define __TARGET__H__
+
+#ifdef TARGET_ESP32
+#include "target_esp32.h"
+#define TARGET_SELECTED
+#endif
+
+#ifdef TARGET_WIN32
+#include "target_win32.h"
+#define TARGET_SELECTED
+#endif
+
+#if !defined(TARGET_SELECTED)
+#error "Target is not selected"
+#endif
+
+#define TARGET_RS485_CFG                             \
+    {                                                \
+        .which_type = CIOT_MSG_DATA_UART_TAG,        \
+        .uart = {                                    \
+            .which_type = CIOT_UART_DATA_CONFIG_TAG, \
+            .config = {                              \
+                .baud_rate = TARGET_RS485_BAUD_RATE, \
+                .num = TARGET_RS485_NUM,             \
+                .gpio.rx = TARGET_RS485_RX_PIN,      \
+                .gpio.tx = TARGET_RS485_TX_PIN,      \
+                .gpio.rts = TARGET_RS485_RTS_PIN,    \
+                .gpio.cts = TARGET_RS485_CTS_PIN,    \
+                .mode = TARGET_RS485_MODE,           \
+                .read_timeout = 0,                   \
+                .write_timeout = 0,                  \
+            },                                       \
+        },                                           \
+    }
+
+#define TARGET_MBUS_SERVER_CFG                              \
+    {                                                       \
+        .which_type = CIOT_MSG_DATA_MBUS_SERVER_TAG,        \
+        .mbus_server = {                                    \
+            .which_type = CIOT_MBUS_SERVER_DATA_CONFIG_TAG, \
+            .config = {                                     \
+                .which_type = CIOT_MBUS_SERVER_CFG_RTU_TAG, \
+                .rtu = {                                    \
+                    .server_id = TARGET_MBUS_SERVER_ID,     \
+                },                                          \
+            },                                              \
+        },                                                  \
+    }
+
+#endif //!__TARGET__H__
