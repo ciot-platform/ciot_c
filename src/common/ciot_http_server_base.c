539f1e4b0aea26ddac19356ab15d85eb3a8d0c8a
wesley-santos <53940899+wjsan@users.noreply.github.com>
2026-09-09 11:23:48 -0300

    feat(http-server): add upload API support in HTTP server

--- src/common/ciot_http_server_base.c
+++ src/common/ciot_http_server_base.c
@@ -29,6 +29,7 @@ ciot_err_t ciot_http_server_init(ciot_http_server_t self)
     base->iface.send_data = ciot_http_server_send_data;
     base->iface.info.type = CIOT_IFACE_TYPE_HTTP_SERVER;
     base->custom_api.enabled = false;
+    base->upload_api.enabled = false;
     return CIOT_ERR_OK;
 }
 
@@ -52,6 +53,15 @@ ciot_err_t ciot_http_server_set_custom_api(ciot_http_server_t self, ciot_http_se
     CIOT_ERR_NULL_CHECK(custom_api);
     ciot_http_server_base_t *base = (ciot_http_server_base_t *)self;
     base->custom_api = *custom_api;
+    return CIOT_ERR_OK;
+}
+
+ciot_err_t ciot_http_server_set_upload_api(ciot_http_server_t self, ciot_http_server_upload_api_t *upload_api)
+{
+    CIOT_ERR_NULL_CHECK(self);
+    CIOT_ERR_NULL_CHECK(upload_api);
+    ciot_http_server_base_t *base = (ciot_http_server_base_t *)self;
+    base->upload_api = *upload_api;
     return CIOT_ERR_OK;
 }
 
