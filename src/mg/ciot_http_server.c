8f6e88419ca72cefc5702dd4e8f9f2863b72c229
wesley-santos <53940899+wjsan@users.noreply.github.com>
2026-09-25 10:25:13 -0300

    feat(http-server): implement enhanced response handling and request header retrieval

--- src/mg/ciot_http_server.c
+++ src/mg/ciot_http_server.c
@@ -15,6 +15,7 @@
 
 #include "ciot_http_server.h"
 #include "mongoose.h"
+#include <ctype.h>
 
 struct ciot_http_server
 {
@@ -28,6 +29,9 @@ struct ciot_http_server
 static const char *TAG = "ciot_http_server";
 
 static bool check_method(struct mg_http_message *hm, const char *method);
+static bool ciot_http_server_get_header(const ciot_http_server_request_t *request, const char *name, char *value, size_t value_size);
+static const char *ciot_http_server_status_text(uint16_t status_code);
+static void ciot_http_server_write_response(ciot_http_server_t self, const ciot_http_server_response_t *response);
 static void ciot_http_server_event_handler(struct mg_connection *c, int ev, void *ev_data);
 
 ciot_http_server_t ciot_http_server_new(void *handle)
@@ -73,16 +77,89 @@ ciot_err_t ciot_http_server_stop(ciot_http_server_t self)
 
 ciot_err_t ciot_http_server_send_bytes(ciot_http_server_t self, uint8_t *data, int size)
 {
-    mg_printf(self->conn_tx,
-              "HTTP/1.0 200 OK\r\n"
-#ifdef CIOT_CONFIG_HTTP_SERVER_ALLOW_ORIGIN
-              "Access-Control-Allow-Origin: " CIOT_CONFIG_HTTP_SERVER_ALLOW_ORIGIN "\r\n"
-#endif
-              "Content-Type: octet-stream\r\n"
-              "Content-Length: %d\r\n\r\n",
-              size);
-    mg_send(self->conn_tx, data, size);
+    ciot_http_server_response_t response = {
+        .status_code = 200,
+        .content_type = "application/octet-stream",
+        .body = data,
+        .body_size = size,
+    };
+    return ciot_http_server_send_response(self, &response);
+}
+
+ciot_err_t ciot_http_server_send_response(ciot_http_server_t self, const ciot_http_server_response_t *response)
+{
+    CIOT_ERR_NULL_CHECK(self);
+    CIOT_ERR_NULL_CHECK(response);
+    ciot_http_server_write_response(self, response);
     return CIOT_ERR_OK;
+}
+
+static void ciot_http_server_write_response(ciot_http_server_t self, const ciot_http_server_response_t *response)
+{
+    uint16_t status_code = response->status_code == 0 ? 200 : response->status_code;
+    mg_printf(self->conn_tx, "HTTP/1.0 %u %s\r\n", status_code, ciot_http_server_status_text(status_code));
+#ifdef CIOT_CONFIG_HTTP_SERVER_ALLOW_ORIGIN
+    mg_printf(self->conn_tx, "Access-Control-Allow-Origin: " CIOT_CONFIG_HTTP_SERVER_ALLOW_ORIGIN "\r\n");
+#endif
+    mg_printf(self->conn_tx, "Content-Type: %s\r\n", response->content_type != NULL ? response->content_type : "application/octet-stream");
+    for (size_t i = 0; i < response->headers_count && i < CIOT_HTTP_SERVER_MAX_RESPONSE_HEADERS; i++)
+    {
+        if (response->headers[i].name != NULL && response->headers[i].value != NULL)
+        {
+            mg_printf(self->conn_tx, "%s: %s\r\n", response->headers[i].name, response->headers[i].value);
+        }
+    }
+    mg_printf(self->conn_tx, "Content-Length: %d\r\n\r\n", (int)response->body_size);
+    if (response->body != NULL && response->body_size > 0) mg_send(self->conn_tx, response->body, response->body_size);
+}
+
+static const char *ciot_http_server_status_text(uint16_t status_code)
+{
+    switch (status_code)
+    {
+    case 200: return "OK";
+    case 201: return "Created";
+    case 204: return "No Content";
+    case 400: return "Bad Request";
+    case 401: return "Unauthorized";
+    case 403: return "Forbidden";
+    case 404: return "Not Found";
+    case 409: return "Conflict";
+    case 428: return "Precondition Required";
+    case 429: return "Too Many Requests";
+    default: return "Internal Server Error";
+    }
+}
+
+static bool ciot_http_server_get_header(const ciot_http_server_request_t *request, const char *name, char *value, size_t value_size)
+{
+    if (request == NULL || name == NULL || value == NULL || value_size == 0) return false;
+    struct mg_http_message *hm = request->context;
+    size_t name_len = strlen(name);
+    const struct mg_str *header = NULL;
+    for (size_t i = 0; i < MG_MAX_HTTP_HEADERS && hm->headers[i].name.len > 0; i++)
+    {
+        if (hm->headers[i].name.len != name_len) continue;
+        bool equal = true;
+        for (size_t j = 0; j < name_len; j++)
+        {
+            if (tolower((unsigned char)hm->headers[i].name.buf[j]) != tolower((unsigned char)name[j]))
+            {
+                equal = false;
+                break;
+            }
+        }
+        if (equal)
+        {
+            header = &hm->headers[i].value;
+            break;
+        }
+    }
+    if (header == NULL) return false;
+    size_t size = header->len < value_size - 1 ? header->len : value_size - 1;
+    memcpy(value, header->buf, size);
+    value[size] = '\0';
+    return true;
 }
 
 static bool check_method(struct mg_http_message *hm, const char *method)
@@ -133,6 +210,31 @@ static void ciot_http_server_event_handler(struct mg_connection *c, int ev, void
         struct mg_http_message *hm = (struct mg_http_message *)ev_data;
         mg_http_parse((char *)c->recv.buf, c->recv.len, hm);
         self->conn_tx = c;
+        ciot_http_server_request_t request = {
+            .method = hm->method.buf,
+            .method_len = hm->method.len,
+            .uri = hm->uri.buf,
+            .uri_len = hm->uri.len,
+            .get_header = ciot_http_server_get_header,
+            .context = hm,
+        };
+        if (!c->loc.is_ip6)
+        {
+            snprintf(request.local_address, sizeof(request.local_address), "%u.%u.%u.%u", c->loc.ip[0], c->loc.ip[1], c->loc.ip[2], c->loc.ip[3]);
+        }
+        if (!c->rem.is_ip6)
+        {
+            snprintf(request.remote_address, sizeof(request.remote_address), "%u.%u.%u.%u", c->rem.ip[0], c->rem.ip[1], c->rem.ip[2], c->rem.ip[3]);
+        }
+        if (base->request_interceptor != NULL)
+        {
+            ciot_http_server_response_t response = {0};
+            if (base->request_interceptor(self, &request, &response, base->request_interceptor_args))
+            {
+                ciot_http_server_write_response(self, &response);
+                break;
+            }
+        }
         if (mg_match(hm->uri, mg_str(base->cfg.route), NULL) && check_method(hm, "POST"))
         {
             ciot_iface_send_event_data(&base->iface, CIOT_EVENT_TYPE_MSG, (uint8_t *)hm->body.buf, hm->body.len);
